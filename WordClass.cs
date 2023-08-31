2ce253f40f9c3e3ec9218023feee971c2b17f9b3
Бейфусь Яков <97034377+BeyfusYakov@users.noreply.github.com>
2023-08-31 17:02:54 +0700

    31/08/2023
    31/08/2023

--- WordClass.cs
+++ WordClass.cs
@@ -8,29 +8,91 @@ namespace SlovoZaSlovo
 {
     internal static class WordClass
     {
-        public static List<Answer> Find(ref char[,] Arr, List<string> sList)
+        public static List<Answer> Find(char[,] Arr, List<string> Source) //ref нельзя в локальных функциях?
         {
-            for (int i = 0; i < 5; i++)
+            int rows = Arr.GetLength(0);
+            int cols = Arr.GetLength(1);
+
+            List <Answer> answers = new List <Answer>();
+            for (int i = 0; i < Arr.GetLength(0); i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < Arr.GetLength(1); j++)
                 {
                     {
-
+                        FindLocFunc(new List<Point>() { new Point(i,j)});
                     }
                 }
             }
 
-
-            //if (startX < battleField.GetLength(0) && startY < battleField.GetLength(1))
+            void FindLocFunc(List<Point> lp)
             {
-                // ...
+                if (Source.Contains(ComposeWord(lp))  )
+                {
+                    answers.Add(new Answer { word = ComposeWord(lp), cost = 0, points = lp });
+                }
+
+                Point lastPoint = lp.Last();
+                if (lp.Count<25)
+                {
+                    if(lastPoint.X-1 > 0 && lastPoint.Y-1>0)
+                    {
+                        lp.Add(new Point(lastPoint.X - 1, lastPoint.Y - 1));
+                        FindLocFunc(lp);
+                    }
+                    if (lastPoint.X - 1 > 0)
+                    {
+                        lp.Add(new Point(lastPoint.X - 1, lastPoint.Y));
+                        FindLocFunc(lp);
+                    }
+
+                    if (lastPoint.X - 1 > 0 && lastPoint.Y + 1 < cols)
+                    {
+                        lp.Add(new Point(lastPoint.X - 1, lastPoint.Y + 1));
+                        FindLocFunc(lp);
+                    }
+                    if (lastPoint.Y - 1 > 0)
+                    {
+                        lp.Add(new Point(lastPoint.X, lastPoint.Y - 1));
+                        FindLocFunc(lp);
+                    }
+                    if (lastPoint.Y + 1 < rows)
+                    {
+                        lp.Add(new Point(lastPoint.X, lastPoint.Y + 1));
+                        FindLocFunc(lp);
+                    }
+                    if (lastPoint.X + 1 < rows && lastPoint.Y - 1 > 0)
+                    {
+                        lp.Add(new Point(lastPoint.X + 1, lastPoint.Y - 1));
+                        FindLocFunc(lp);
+                    }
+                    if (lastPoint.X + 1 < rows )
+                    {
+                        lp.Add(new Point(lastPoint.X + 1, lastPoint.Y));
+                        FindLocFunc(lp);
+                    }
+                    if (lastPoint.X + 1 < rows && lastPoint.Y + 1 < cols)
+                    {
+                        lp.Add(new Point(lastPoint.X + 1, lastPoint.Y + 1));
+                        FindLocFunc(lp);
+                    }
+                }
             }
 
-            Answer a = new Answer { cost =10, Points = new List<Point>(), word = "перчик"};
-            
-        
+            string ComposeWord (List<Point> lp)
+            {
+                StringBuilder sb = new StringBuilder(25);
+                foreach (Point p in lp) sb.Append(Arr[p.X,p.Y]);
+                return sb.ToString();
+            }
 
-            return new List<Answer> { a};
+
+
+
+            answers.Add(new Answer { cost = 10, points = new List<Point>(), word = "алюляка" });
+            return answers;
         }
+
+
+
     }
 }
