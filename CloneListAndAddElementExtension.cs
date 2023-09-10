c40c22ea98ce4938ed944511c8e81eefe49dae5b
bejaw <beyfus.Yakov@gmail.com>
2023-09-10 20:31:39 +0700

    добавил вариантКоллекшн. Заменить CloneAndAdd

---
+++ CloneListAndAddElementExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlovoZaSlovo
+{
+    internal static class ListExtension
+    {
+        public static List<T> CLoneAndAdd<T>(this List<T> source, T a)
+        {
+            List<T> tempList = new List<T>();
+            
+            tempList = source.GetRange(0, source.Count);
+            tempList.Add(a);
+            return tempList;
+        }
+
+        public static List<T> CLone<T>(this List<T> source)
+        {
+            return source.GetRange(0, source.Count);
+        }
+    }
+}
