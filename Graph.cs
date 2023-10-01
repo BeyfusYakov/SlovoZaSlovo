6c201f92e7e01a6de17875feae5275bf157b46cb
bejaw <beyfus.Yakov@gmail.com>
2023-10-01 22:09:08 +0700

    01/10/2023

---
+++ Graph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlovoZaSlovo
+{
+    internal class Graph
+    {
+        Point[,] points = new Point[5,5] ;
+        List<Edge> edges = new();
+        public void AddEdges(Point from, Point to)
+        {
+            edges.Add(new Edge(from, to));
+        }
+
+        Graph(Point[,] points, List<Edge> edges)
+        {
+            this.points = points ;
+            this.edges = edges ;
+        }
+
+
+        public List<Vertex> GetVetexLists(Vertex vertex)
+        {
+            var result = new List<Vertex>();
+
+            foreach (var edge in Edges)
+            {
+                if (edge.From == vertex)
+                {
+                    result.Add(edge.To);
+                }
+            }
+
+            return result;
+        }
+    }
+}
